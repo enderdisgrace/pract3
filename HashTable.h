8aada61fca16b49995f3a01543d85f1e8dbee2fb
enderdisgrace <145371712+enderdisgrace@users.noreply.github.com>
2024-11-26 21:20:39 +0100

    Add files via upload

---
+++ HashTable.h
@@ -0,0 +1,71 @@
+#ifndef HASHTABLE_H
+#define HASHTABLE_H
+
+#include <ostream>
+#include <stdexcept>
+#include "Dict.h"
+#include "TableEntry.h"
+
+template <typename V>
+class HashTable: public Dict<V> {
+
+    private:
+        int n,max;
+        std::list<TableEntry<V>>* table;
+        int h(std::string key) {
+            int sum = 0;
+            for (char c : key) {
+                sum += int(c); 
+            }
+            return sum % max; 
+        }
+
+
+    public:
+        HashTable(int size) : n(0), max(size) {
+            table = new std::list<TableEntry<V>>[max]; ;
+        }
+
+        ~HashTable(){
+            delete[] table;
+        }
+
+        int capacity(){
+            return max;
+        }
+
+    V operator[](std::string key) {
+        int index = h(key); 
+        
+        for (int i = 0; i < table[index].size(); ++i) { 
+            
+            if (table[index][i] == key) { 
+                return table[index][i].value; 
+            }
+        }
+        throw std::runtime_error("Key not found");
+    }
+
+
+        
+    friend std::ostream& operator<<(std::ostream &out, const HashTable<V> &ht) {
+        for (int i = 0; i < ht.max; ++i) { 
+            out << "Bucket " << i << ": "; 
+        
+            for (int j = 0; j < ht.table[i].size(); ++j) {
+                out << ht.table[i][j] << " ";
+            }
+            
+            out << "\n"; 
+        }
+        return out;
+    }
+
+
+        
+};
+
+#endif
+
+
+
